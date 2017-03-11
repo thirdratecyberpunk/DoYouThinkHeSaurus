25fdd49902d4ba3e47f194cd74c9ed34810130bb
Lewis Blackburn <blackbul@aston.ac.uk>
2017-03-11 17:53:19 +0000

    docToString.php now opens the .xml file

---
+++ docToString.php
@@ -0,0 +1,23 @@
+<?php
+// turns a .docx file passed by the user into an array of strings
+// turns the file into a zip archive
+$zip = new ZipArchive;
+$res = $zip->open('test.docx');
+if ($res === TRUE) {
+  $zip->extractTo('extractedFiles');
+  $zip->close();
+  echo 'Extracted .docx file!' . "\n";
+} else {
+  echo 'Cannot extract file!' . "\n";
+  exit();
+}
+// loads the contents of document.xml
+$fileName = "extractedFiles/word/document.xml";
+$myfile = fopen( $fileName, "r") or die("Unable to open file!" . "\n");
+$contents =  fread($myfile,filesize($fileName));
+fclose($myfile);
+
+// breaks up contents
+echo ($contents . "/n");
+print_r (explode(">",$contents));
+ ?>
