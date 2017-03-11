14ad95a60093182fe506ef4487dac7aa57623265
Lewis Blackburn <blackbul@aston.ac.uk>
2017-03-11 19:35:06 +0000

    Changed docToString.php into a function

--- docToString.php
+++ docToString.php
@@ -1,22 +1,24 @@
 <?php
 // turns a .docx file passed by the user into an array of strings
-// turns the file into a zip archive
-$zip = new ZipArchive;
-$res = $zip->open('test.docx');
-if ($res === TRUE) {
-  $zip->extractTo('extractedFiles');
-  $zip->close();
-  echo 'Extracted .docx file!' . "\n";
-} else {
-  echo 'Cannot extract file!' . "\n";
-  exit();
-}
-// loads the contents of document.xml
-$fileName = "extractedFiles/word/document.xml";
-$myfile = fopen( $fileName, "r") or die("Unable to open file!" . "\n");
-$contents =  fread($myfile,filesize($fileName));
-fclose($myfile);
+function getTextLines($filename){
+  $zip = new ZipArchive;
+  $res = $zip->open($filename);
+  if ($res === TRUE) {
+    $zip->extractTo('extractedFiles');
+    $zip->close();
+    echo 'Extracted .docx file!' . "\n";
+  }
+  else {
+    echo 'Cannot extract file!' . "\n";
+    return null;
+  }
+  $fileName = "extractedFiles/word/document.xml";
+  $myfile = fopen( $fileName, "r") or die("Unable to open file!" . "\n");
+  $contents =  fread($myfile,filesize($fileName));
+  fclose($myfile);
 
-preg_match_all('/<w:t>[^<>]+<\/w:t>/', $contents, $wordXML);
-print_r ($wordXML);
+  preg_match_all('/<w:t>[^<>]+<\/w:t>/', $contents, $wordXML);
+  return $wordXML;
+  }
+}
  ?>
