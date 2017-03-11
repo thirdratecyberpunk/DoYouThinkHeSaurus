3095e2538c62d7e7b179f4a703d986942faee1d0
Jenny Myers <jenny.myers@btinternet.com>
2017-03-11 22:24:47 +0000

    fish

--- pullFromIndex.php
+++ pullFromIndex.php
@@ -1,6 +1,10 @@
-<?php>
-/* adds to the document index in algolia */
+<?php
+/* indexes the document index in algolia */
 $index = $client->initIndex('document');
 $batch = json_decode(file_get_contents('document.json'), true);
 $index->addObjects($batch);
+
+/*gets the text in the search bar, which has been typed in and searches the document for it*/
+$search = $_POST[/*name of search text in form*/];
+var_dump($index->search());
 ?>
