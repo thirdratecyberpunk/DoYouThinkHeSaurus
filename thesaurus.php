361e9c967c9510d5cd39557879e3566a8887aa7d
Jenny Myers <jenny.myers@btinternet.com>
2017-03-12 09:37:14 +0000

    thesaurus base

---
+++ thesaurus.php
@@ -0,0 +1,6 @@
+<?php
+$apikey = "pdIeoum1yDzxVgHwxWib"; // API key
+$word =//needs to get the word they search; // any word
+$language = "en_US"; //sets language to english
+$endpoint = "http://thesaurus.altervista.org/thesaurus/v1";
+ ?>
